a09e9262bec33b4bb71f43d32028fde26f43e586
Nicolas Boisselier <nicolas.boisselier@gmail.com>
2016-10-07 23:23:35 +0100

    before restore stable

--- manifests/default.pp
+++ manifests/default.pp
@@ -52,42 +52,67 @@ package {[
   'wget',
   'debconf-utils',
 ]:
-  ensure       => 'latest',
+  ensure => 'latest',
 }
-->
-exec { '/usr/bin/wget http://nginxlibrary.com/downloads/perl-fcgi/fastcgi-wrapper -O /usr/bin/fastcgi-wrapper.pl && /bin/chmod 0755 /usr/bin/fastcgi-wrapper.pl':
-  creates => '/usr/bin/fastcgi-wrapper.pl'
+->exec { 'wget http://nginxlibrary.com/downloads/perl-fcgi/fastcgi-wrapper -O /usr/bin/fastcgi-wrapper.pl && chmod 0755 /usr/bin/fastcgi-wrapper.pl':
+  creates => '/usr/bin/fastcgi-wrapper.pl',
+  notify  => Service['perl-fcgi'],
 }
-->
-service { [
-  'nginx',
-  'perl-fcgi',
-]:
-  ensure => 'running'
+->class { 'mysql_server': password => $mysql_password }
+
+
+service { [ 'nginx' ]:
+  ensure  => 'running',
+  require => Service['perl-fcgi'],
 }
-->
-file { '/etc/debconf-set-selections.txt':
-  mode    => '0600',
-  content => "
-mysql-server-5.5        mysql-server/root_password               password ${mysql_password}
-mysql-server-5.5        mysql-server/root_password_again         password ${mysql_password}
-"
+
+service { 'perl-fcgi':
+  ensure => 'running',
 }
-->
-package { 'mysql-server' :
-  ensure          => 'present',
-  responsefile    => '/etc/debconf-set-selections.txt',
-}
-->
-file { '/root/.my.cnf':
-  content => "### MANAGED BY PUPPET ###
-[client]
-password=${mysql_password}
-"
-}
-# NB 07.10.16 ->
-# NB 07.10.16 exec { 'mysqlpasswd':
-# NB 07.10.16   command => "mysqladmin -u root password ${mysql_password}",
-# NB 07.10.16   unless => "mysqladmin -uroot -p${mysql_password} status",
-# NB 07.10.16 }
 # NB 07.10.16 ->exec { 'debconf-set-selections /etc/debconf-set-selections.txt && apt-get install -qqy mysql-server': environment => 'DEBIAN_FRONTEND=noninteractive',path=>'/bin:/usr/bin:/sbin:/usr/sbin'}
+
+class mysql_server (
+  $ensure   = 'present',
+  $password = '',
+  $bind_address = '0.0.0.0'
+) {
+
+  file { '/etc/debconf-set-selections.txt':
+    mode    => '0600',
+    content => "
+mysql-server-5.5        mysql-server/root_password               password ${password}
+mysql-server-5.5        mysql-server/root_password_again         password ${password}
+"
+  }
+
+  package { 'mysql-server' :
+    ensure         => 'present',
+    responsefile  => '/etc/debconf-set-selections.txt',
+    require       => File['/etc/debconf-set-selections.txt'],
+  }
+
+if (false) {
+  file { '/etc/mysql/conf.d/bind-address.cfg':
+    require => Package['mysql-server'],
+    notify  => Service['mysql'],
+    content => "### MANAGED BY PUPPET ###\n[mysqld]\nbind-address = ${bind_address}\n"
+  }
+}
+
+  exec { 'mysqlpasswd':
+    command => "mysqladmin -u root password ${mysql_password}",
+    unless  => "mysqladmin -uroot -p${mysql_password} status",
+    require => Service['mysql'],
+  }
+
+  file { '/root/.my.cnf':
+    require => Exec['mysqlpasswd'],
+    content => "### MANAGED BY PUPPET ###\n[client]\npassword=${password}\n",
+  }
+
+  service { 'mysql':
+    ensure  => 'running',
+    require => Package['mysql-server'],
+  }
+
+}
